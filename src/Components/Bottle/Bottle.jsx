c8ec7b205051b27f21fa73b074f3eb009d288d36
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-03 19:42:07 +0600

    add to cart option added'

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleCart}) => {
+    const {name, seller, stock, price} = bottle; 
     return (
-        <div>
+        <div style={{textAlign: 'left', border: '2px solid gray', borderRadius: '10px', padding:'20px'}}>
             <img style={{width: '100%', borderRadius: '8px' }} src={bottle.img} alt="" />
-            <h4>{bottle.name}</h4>
-            <h5>Brand: {bottle.seller}</h5>
+            <h4>{name}</h4>
+            <h5 style={{margin: '0'}}>Brand: {seller}</h5>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <h4>Price: ${price}</h4>
+                <p>Available: {stock}</p>
+            </div>
+            <button onClick={()=>{handleCart(bottle)}}>Add to cart</button>
         </div>
     );
 };
--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,22 +1,41 @@
-import React, { use } from 'react';
-import Bottle from '../bottle/bottle';
+import React, { use, useState } from "react";
+import Bottle from "../bottle/bottle";
 
-const Bottles = ({bottleFetch}) => {
-    const bottles = use(bottleFetch);
-    
-    return (
-        <div>
-            <h2>Total Bottles: {bottles.length}</h2>
-            <div style={{display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '24px'}}>
-                {
-                    bottles.map( bottle => <Bottle
-                        key={bottle.id} 
-                        bottle = {bottle}
-                        ></Bottle> )
-                }
-            </div>
-        </div>
-    );
+const Bottles = ({ bottleFetch }) => {
+  const bottles = use(bottleFetch);
+  const [cart, setCart] = useState([]);
+  const [price, setPrice] = useState(0);
+
+  const handleCart = (cartItem) => {
+    const newCart = [...cart, cartItem];
+    setCart(newCart)
+    const total = price+cartItem.price;
+    setPrice(total);
+  };
+
+  return (
+    <div>
+      <div style={{display: 'flex', justifyContent: 'space-between', borderBottom: '2px solid gray', marginBottom: '20px'}}>
+        <h2>Total Bottles: {bottles.length}</h2>
+        <h2>Cart: {cart.length}</h2>
+        <h2>Total Price: ${price}</h2>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "24px",
+        }}
+      >
+        {bottles.map((bottle) => (
+            <Bottle 
+            key={bottle.id} 
+            bottle={bottle}
+            handleCart = {handleCart}></Bottle>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Bottles;
